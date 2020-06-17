b3a01c4687ad3acc35d60607ad331cc6a2afcd1f
shahar <shaharp2@gmail.com>
2020-06-17 16:15:01 +0300

    sffs

--- src/services/book.service.js
+++ src/services/book.service.js
@@ -1,5 +1,7 @@
 
+import books from '../../public/books.json'
 
+let gBooks = books
 
 export default {
     query,
@@ -7,78 +9,11 @@ export default {
 }
 
 function query() {
-    return books
+    return gBooks
 }
 function getById(id) {
 
-    return books.find(book => book.id === id)
+    return gBooks.find(book => book.id === id)
 
 }
 
-let books = [{
-    "id": "GXj93KOkqZoC",
-    "title": "Hacking",
-    "subtitle": "Digital Media and Technological Determinism",
-    "authors": [
-        "Tim Jordan",
-        "Puki Ben David"
-    ],
-    "publishedDate": 2008,
-    "description": "Hacking provides an introduction to the community of hackers and an analysis of the meaning of hacking in twenty-first century societies.",
-    "pageCount": 160,
-    "categories": [
-        "Computers",
-        "Hack"
-    ],
-    "thumbnail": " http://coding-academy.org/books-photos/1.jpg",
-    "language": "en",
-    "listPrice": {
-        "amount": 19,
-        "currencyCode": "ILS",
-        "isOnSale": true
-    }
-}, {
-    "id": "GXj93KOkqZoC",
-    "title": "Hacking",
-    "subtitle": "Digital Media and Technological Determinism",
-    "authors": [
-        "Tim Jordan",
-        "Puki Ben David"
-    ],
-    "publishedDate": 2008,
-    "description": "Hacking provides an introduction to the community of hackers and an analysis of the meaning of hacking in twenty-first century societies.",
-    "pageCount": 160,
-    "categories": [
-        "Computers",
-        "Hack"
-    ],
-    "thumbnail": " http://coding-academy.org/books-photos/1.jpg",
-    "language": "en",
-    "listPrice": {
-        "amount": 19,
-        "currencyCode": "ILS",
-        "isOnSale": true
-    }
-}, {
-    "id": "GXj93KOkqZoC",
-    "title": "Hacking",
-    "subtitle": "Digital Media and Technological Determinism",
-    "authors": [
-        "Tim Jordan",
-        "Puki Ben David"
-    ],
-    "publishedDate": 2008,
-    "description": "Hacking provides an introduction to the community of hackers and an analysis of the meaning of hacking in twenty-first century societies.",
-    "pageCount": 160,
-    "categories": [
-        "Computers",
-        "Hack"
-    ],
-    "thumbnail": " http://coding-academy.org/books-photos/1.jpg",
-    "language": "en",
-    "listPrice": {
-        "amount": 19,
-        "currencyCode": "ILS",
-        "isOnSale": true
-    }
-}]

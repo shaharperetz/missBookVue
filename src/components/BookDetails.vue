f400d47794fc0db67a9c18ca638b35e7c15a5c70
AbirNadav <pakakaw41@gmail.com>
2020-06-17 15:43:17 +0300

    123

---
+++ src/components/BookDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="book-details-container flex a-center j-center wrap">
+    <h2>{{book.title}}</h2>
+    <img :src="book.thumbnail" alt srcset />
+    <span>{{book.publishedDate}}</span>
+    <p>{{book.description}}</p>
+  </div>
+</template>
+
+<script>
+import bookService from "../services/book.service";
+
+export default {
+  data() {
+    return {
+      currBook: null
+    };
+  },
+  name: "BookDetails",
+  props: {
+    msg: String
+  },
+  components: {}
+};
+</script>
+
+<style scoped lang="scss">
+</style>
